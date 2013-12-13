9db6dcf7d96ff17b8e7181dfa1c25ead3dd0842f
th0mas-87 <epic.th0mas87@gmail.com>
2013-12-13 15:26:07 -0700

    Added about section stuff...

--- webstrike.js
+++ webstrike.js
@@ -2,7 +2,7 @@
 $(document).ready(function() {
 	$('section').css('height', $(window).height() + 'px');
 	$('section.white h1').fitText(1.75);
-	$('section.white p').fitText(3.5);
+	$('section.white p').fitText(4);
 	$('section#welcome h1').fitText(0.75);
 	$('section#welcome h2').fitText(2);
 	$('section#welcome h3').fitText(6.75);
@@ -11,9 +11,14 @@ $(document).ready(function() {
 	$('section#welcome img').css('width', $(window).width() / 20 + 'px');
 	$('section#welcome img').css('height', $(window).height() / 20 + 'px');
 	$('section#welcome img').css('padding-top', ($(window).height() - $('section#welcome div').outerHeight()) - $(window).height() / 20 * 2.75 + 'px');
+	$('section#about div#services h3').fitText(1.125);
+	$('section#about div#services span').fitText(1.375);
 	$('section#approach h1').fitText(1.25);
 	$('section#approach h2').fitText(4.5);
 	$('section#approach div').css('padding-top', ($(window).height() - $('section#approach div').height()) / 2 + 'px');
+	$('section.white').css('height', $('section.white div#content').height() + 'px');
+	$('section.white').css('padding-top', ($(window).height() - $('section.white div#content').height()) / 4);
+	$('section.white').css('padding-bottom', ($(window).height() - $('section.white div#content').height()) / 4 * 3);
 	$('div#main').onepage_scroll({
 		sectionContainer: 'section',
 		easing: 'ease',
@@ -42,4 +47,6 @@ $(window).resize(function() {
 	$('section#welcome img').css('height', $(window).height() / 20 + 'px');
 	$('section#welcome img').css('padding-top', ($(window).height() - $('section#welcome div').outerHeight()) - $(window).height() / 20 * 2.75 + 'px');
 	$('section#approach div').css('padding-top', ($(window).height() - $('section#approach div').height()) / 2 + 'px');
+	$('section.white').css('padding-top', ($(window).height() - $('section.white div#content').height()) / 4);
+	$('section.white').css('padding-bottom', ($(window).height() - $('section.white div#content').height()) / 4 * 3);
 });
